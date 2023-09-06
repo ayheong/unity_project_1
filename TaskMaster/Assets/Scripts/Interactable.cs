a0e90aa92bef757f658a93b422c7ef7220174fdf
Tyger8540 <tymatson2015@gmail.com>
2023-09-05 17:59:52 -0700

    text pops up when click on circle, but have to be in line with center

--- TaskMaster/Assets/Scripts/Interactable.cs
+++ TaskMaster/Assets/Scripts/Interactable.cs
@@ -4,10 +4,26 @@ using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
-public abstract class Interact : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
-    public Rigidbody2D playerBody;
-   
+    public GameObject textPanel;
+    public float distance = 3f;
 
+    private void OnMouseUpAsButton()
+    {
+        if (textPanel.activeSelf)
+        {
+            textPanel.SetActive(false);
+        }
+        else if (Physics2D.Raycast(transform.position, Vector2.right, distance, LayerMask.GetMask("Player")) ||
+                 Physics2D.Raycast(transform.position, Vector2.left, distance, LayerMask.GetMask("Player")) ||
+                 Physics2D.Raycast(transform.position, Vector2.up, distance, LayerMask.GetMask("Player")) ||
+                 Physics2D.Raycast(transform.position, Vector2.down, distance, LayerMask.GetMask("Player")))
+        {
+            textPanel.SetActive(true);
+        }
+    }
 }

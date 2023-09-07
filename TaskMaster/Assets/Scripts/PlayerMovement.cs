cbed8596e580f3da7c4fa79e01323412ede071bd
Tyger8540 <tymatson2015@gmail.com>
2023-09-07 16:27:20 -0700

    Ty's Test Scene added with character animations

--- TaskMaster/Assets/Scripts/PlayerMovement.cs
+++ TaskMaster/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed;
     
     public Rigidbody2D playerBody;
+
+    public Animator animator;
+
+    private float last_direction_x;
+    private float last_direction_y;
     
     Vector2 movement;
     private void Start()
@@ -20,7 +25,35 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if(Input.GetKey(KeyCode.LeftShift))
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x > 0.01)
+        {
+            animator.SetFloat("Last_Direction_X", 1);
+            animator.SetFloat("Last_Direction_Y", 0);
+        }
+        else if(movement.x < -0.01)
+        {
+            animator.SetFloat("Last_Direction_X", -1);
+            animator.SetFloat("Last_Direction_Y", 0);
+        }
+
+
+        if(movement.y > 0.01)
+        {
+            animator.SetFloat("Last_Direction_Y", 1);
+            animator.SetFloat("Last_Direction_X", 0);
+        }
+        else if (movement.y < -0.01)
+        {
+            animator.SetFloat("Last_Direction_Y", -1);
+            animator.SetFloat("Last_Direction_X", 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             moveSpeed = initialMoveSpeed * 2;
         }
